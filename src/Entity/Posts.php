f2a7115af90e0a3b7e636bb5cbe7cdbf508ff285
Bocar Gueye <bgueye@gmail.com>
2018-03-22 19:36:51 +0100

    first commit kw29

---
+++ src/Entity/Posts.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PostsRepository")
+ */
+class Posts
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $texte;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(?string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getTexte(): ?text
+    {
+        return $this->texte;
+    }
+
+    public function setTexte(?text $texte): self
+    {
+        $this->texte = $texte;
+
+        return $this;
+    }
+}
